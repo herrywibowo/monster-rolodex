d7130460b65ad3e494e6f0384567c58069c90517
herryw <herry.owok@outlook.com>
2020-03-03 09:21:54 +0700

    Updates

---
+++ precache-manifest.84f0f57611a448641e8e58405f052e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792f8528f77f8af47d9915c7b782eb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44231831f52455e8c8f1",
+    "url": "/static/css/main.6f9d65c0.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44231831f52455e8c8f1",
+    "url": "/static/js/main.96c9ce72.chunk.js"
+  },
+  {
+    "revision": "95b00f7e967302e9ee68",
+    "url": "/static/js/runtime-main.7873e549.js"
+  }
+]);
